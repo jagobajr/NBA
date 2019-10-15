f2253f779552203366c54a4bf0f45fd5e62f0302
Jagoba <jagobagomez@opendeusto.es>
2019-10-15 08:23:22 +0200

    Yas

--- src/NBA/VentanaLogin.java
+++ src/NBA/VentanaLogin.java
@@ -9,10 +9,14 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import java.awt.FlowLayout;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
 
 public class VentanaLogin extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -37,12 +41,38 @@ public class VentanaLogin extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		contentPane.setBackground(UIManager.getColor("PasswordField.selectionBackground"));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		contentPane.setLayout(null);
 		
 		JButton btnLogIn = new JButton("Log In");
+		btnLogIn.setBackground(UIManager.getColor("PopupMenu.selectionBackground"));
+		btnLogIn.setBounds(31, 195, 83, 29);
 		contentPane.add(btnLogIn);
+		
+		JLabel lblUsuario = new JLabel("Usuario:");
+		lblUsuario.setBounds(31, 43, 61, 16);
+		contentPane.add(lblUsuario);
+		
+		textField = new JTextField();
+		textField.setBackground(UIManager.getColor("PopupMenu.selectionBackground"));
+		textField.setBounds(31, 72, 130, 26);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblContrasea = new JLabel("Contraseña:");
+		lblContrasea.setBounds(31, 110, 105, 16);
+		contentPane.add(lblContrasea);
+		
+		textField_1 = new JTextField();
+		textField_1.setBackground(UIManager.getColor("PopupMenu.selectionBackground"));
+		textField_1.setBounds(31, 138, 130, 26);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel label = new JLabel("");
+		label.setBounds(287, 77, 61, 16);
+		contentPane.add(label);
 	}
-
 }
