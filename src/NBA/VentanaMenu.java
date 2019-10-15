7d640320c5260362d6115502e011e7419c4cc622
Jagoba <jagobagomez@opendeusto.es>
2019-10-15 09:32:27 +0200

    VAMOS

--- src/NBA/VentanaMenu.java
+++ src/NBA/VentanaMenu.java
@@ -6,6 +6,13 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class VentanaMenu extends JFrame {
 
@@ -31,12 +38,49 @@ public class VentanaMenu extends JFrame {
 	 * Create the frame.
 	 */
 	public VentanaMenu() {
+		setBackground(new Color(105, 105, 105));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 376, 405);
+		setBounds(100, 100, 230, 405);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(128, 128, 128));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblMenu = new JLabel("MENU");
+		lblMenu.setFont(new Font("Gurmukhi MN", Font.BOLD, 16));
+		lblMenu.setForeground(new Color(248, 248, 255));
+		lblMenu.setBounds(82, 24, 142, 47);
+		contentPane.add(lblMenu);
+		
+		JButton btnClasificacion = new JButton("CLASIFICACION");
+		btnClasificacion.setEnabled(false);
+		btnClasificacion.setBackground(Color.DARK_GRAY);
+		btnClasificacion.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnClasificacion.setForeground(Color.WHITE);
+		btnClasificacion.setBounds(6, 95, 218, 57);
+		contentPane.add(btnClasificacion);
+		
+		JButton btnEquipo = new JButton("PLANTILLA");
+		btnEquipo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnEquipo.setEnabled(false);
+		btnEquipo.setBounds(6, 147, 218, 47);
+		contentPane.add(btnEquipo);
+		
+		JButton btnMercado = new JButton("MERCADO");
+		btnMercado.setEnabled(false);
+		btnMercado.setBounds(6, 189, 218, 57);
+		contentPane.add(btnMercado);
+		
+		JButton btnCerrarSesion = new JButton("CERRAR SESION");
+		btnCerrarSesion.setBounds(43, 335, 142, 42);
+		contentPane.add(btnCerrarSesion);
 	}
 
 }
