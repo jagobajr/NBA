3ac5559af15715fd65159da8411ec9470f4ab305
Jagoba <jagobagomez@opendeusto.es>
2019-10-15 09:08:42 +0200

    sapos

--- src/NBA/VentanaLogin.java
+++ src/NBA/VentanaLogin.java
@@ -11,6 +11,8 @@ import javax.swing.JLabel;
 import java.awt.FlowLayout;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
+import javax.swing.JProgressBar;
+import javax.swing.ImageIcon;
 
 public class VentanaLogin extends JFrame {
 
@@ -39,7 +41,7 @@ public class VentanaLogin extends JFrame {
 	 */
 	public VentanaLogin() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 376, 405);
 		contentPane = new JPanel();
 		contentPane.setBackground(UIManager.getColor("PasswordField.selectionBackground"));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -48,31 +50,36 @@ public class VentanaLogin extends JFrame {
 		
 		JButton btnLogIn = new JButton("Log In");
 		btnLogIn.setBackground(UIManager.getColor("PopupMenu.selectionBackground"));
-		btnLogIn.setBounds(31, 195, 83, 29);
+		btnLogIn.setBounds(6, 207, 83, 29);
 		contentPane.add(btnLogIn);
 		
 		JLabel lblUsuario = new JLabel("Usuario:");
-		lblUsuario.setBounds(31, 43, 61, 16);
+		lblUsuario.setBounds(6, 43, 61, 16);
 		contentPane.add(lblUsuario);
 		
 		textField = new JTextField();
 		textField.setBackground(UIManager.getColor("PopupMenu.selectionBackground"));
-		textField.setBounds(31, 72, 130, 26);
+		textField.setBounds(6, 72, 300, 36);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
 		JLabel lblContrasea = new JLabel("Contraseña:");
-		lblContrasea.setBounds(31, 110, 105, 16);
+		lblContrasea.setBounds(6, 119, 105, 16);
 		contentPane.add(lblContrasea);
 		
 		textField_1 = new JTextField();
 		textField_1.setBackground(UIManager.getColor("PopupMenu.selectionBackground"));
-		textField_1.setBounds(31, 138, 130, 26);
+		textField_1.setBounds(6, 150, 300, 26);
 		contentPane.add(textField_1);
 		textField_1.setColumns(10);
 		
 		JLabel label = new JLabel("");
 		label.setBounds(287, 77, 61, 16);
 		contentPane.add(label);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon("/Users/jagoba.jr/Desktop/DEUSTO📚💎/19-20/Programacion/Imagenes para el proyecto/LOGO.png"));
+		lblNewLabel.setBounds(158, 106, 210, 305);
+		contentPane.add(lblNewLabel);
 	}
 }
