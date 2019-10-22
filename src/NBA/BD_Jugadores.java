3c89a427259826f0470e5429b665dc279c1cff41
Jagoba <jagobagomez@opendeusto.es>
2019-10-22 14:09:56 +0200

    Base de Datos jugadores _1

---
+++ src/NBA/BD_Jugadores.java
@@ -0,0 +1,42 @@
+package NBA;
+
+import java.sql.*;
+
+public class BD_Jugadores {
+
+	public static void main(String[]args) {
+		try {
+		Class.forName("org.sqlite.JDBC");
+
+
+		Connection conn = DriverManager.getConnection("jdbc:sqlite:data/NBA.db");
+		Statement stmt = conn.createStatement();
+		// conexion abierta
+		// statement creado
+		ResultSet rs = stmt.executeQuery("SELECT Nombre, Posicion, Equipo, Puntos FROM Jugadores");
+
+		while(rs.next()) {
+		String nombre = rs.getString("Nombre");
+		String Posicion = rs.getString("Posicion");
+		String Equipo = rs.getString("Equipo");
+		String Puntos = rs.getString("Puntos");
+ 
+
+		System.out.println("Nombre: " + nombre + " Posicion: " + Posicion + " Equipo: " + Equipo + " Puntos: " + Puntos); 
+		}
+
+		//ya no queremos usarla mas
+
+		stmt.close();
+		conn.close();
+
+		}catch(ClassNotFoundException e) {
+		System.out.println("No se ha podido cargar el driver");
+		}catch(SQLException e) {
+		System.out.println("No se ha podido cargar la BD");
+		}
+
+
+		}
+		}
+//+ "Puntos: " + Puntos);
