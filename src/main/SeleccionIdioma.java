2933f89fe8ec5b669698f68daafc0d501a23441d
alfon <alfon@192.168.1.253>
2020-01-12 12:41:58 +0100

    la clase echa

---
+++ src/main/SeleccionIdioma.java
@@ -0,0 +1,49 @@
+package main;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JSplitPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.beans.Statement;
+import java.io.Serializable;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.logging.Level;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import ventanas.VentanaMenu;
+import ventanas.VentanaMercado;
+
+
+import main.Usuarios;
+
+import java.awt.Color;
+import java.awt.Label;
+import java.awt.Panel;
+import java.awt.Font;
+
+import javax.swing.AbstractButton;
+import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
+
+import java.sql.*;
+public class SeleccionIdioma {
+
+}
